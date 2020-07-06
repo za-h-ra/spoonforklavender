8ddf437d0bc357a6911ecbbe0b6ed3f7295de5e9
zahrakhadijha <zahra.khan1@icloud.com>
2020-07-06 12:36:02 -0400

    deploy site

---
+++ build/precache-manifest.8810c83617c9b379c160f00d125e4f5f.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7fe157da7722a2b30d27abdcc394e4d8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4258eef1897115253c35",
+    "url": "/static/css/main.88610ed5.chunk.css"
+  },
+  {
+    "revision": "94887169444e6662cea1",
+    "url": "/static/js/2.18909665.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.18909665.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4258eef1897115253c35",
+    "url": "/static/js/main.8402afb9.chunk.js"
+  },
+  {
+    "revision": "3be0ada8eaaab1cb6421",
+    "url": "/static/js/runtime-main.f3c58923.js"
+  },
+  {
+    "revision": "42b4e1074999d2dccff3ee0a898fcc1b",
+    "url": "/static/media/Logo-Black.42b4e107.png"
+  },
+  {
+    "revision": "066e44da0f50e7086ba69f964e675fe4",
+    "url": "/static/media/Logo-White-2.066e44da.png"
+  },
+  {
+    "revision": "f604886af276f48640d9317779e7703f",
+    "url": "/static/media/SquidInk.f604886a.jpg"
+  },
+  {
+    "revision": "33504058fc885c6945012b9d9f7719f5",
+    "url": "/static/media/pasta.33504058.jpg"
+  }
+]);
